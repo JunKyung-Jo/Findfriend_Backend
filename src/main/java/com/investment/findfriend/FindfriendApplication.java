272b040eb6460f4224ad1d71d384097b70dafbb1
NameIsUser06 <woosoungcho@gmail.com>
2023-11-16 13:44:51 +0900

    INIT : Initalize

---
+++ src/main/java/com/investment/findfriend/FindfriendApplication.java
@@ -0,0 +1,13 @@
+package com.investment.findfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindfriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindfriendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/investment/findfriend/FindfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment.findfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindfriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
