388785d5215eb65cdd0920028d398ac6152c3a79
NameIsUser06 <woosoungcho@gmail.com>
2023-11-16 13:58:41 +0900

    feat : User domain 구현

---
+++ src/main/java/com/investment/findfriend/domain/user/domain/User.java
@@ -0,0 +1,42 @@
+package com.investment.findfriend.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private LocalDate birthdate;
+
+    @Column
+    private Gender gender;
+
+    @Column
+    private Authority authority;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String profile;
+}
