1cbe8635d314532304dd6f3770f75972a2fb61b1
NameIsUser06 <woosoungcho@gmail.com>
2023-12-04 10:18:31 +0900

    refactor : feign 리팩토링

--- src/main/java/com/investment/findfriend/global/feign/google/GoogleGetUserInfoClient.java
+++ src/main/java/com/investment/findfriend/global/feign/google/GoogleGetUserInfoClient.java
@@ -1,16 +1,16 @@
-package com.investment.findfriend.global.auth.google;
+package com.investment.findfriend.global.feign.google;
 
 
-import com.investment.findfriend.global.auth.dto.response.google.GoogleUserInfoResponse;
+import com.investment.findfriend.global.feign.dto.response.google.GoogleUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
-        name = "GetUserInfoService",
+        name = "GoogleGetUserInfoClient",
         url = "https://www.googleapis.com/userinfo/v2/me"
 )
-public interface GoogleGetUserInfoService {
+public interface GoogleGetUserInfoClient {
     @GetMapping
     GoogleUserInfoResponse execute(@RequestParam String access_token);
 }
---
+++ src/main/java/com/investment/findfriend/global/feign/naver/NaverGetTokenClient.java
@@ -0,0 +1,15 @@
+package com.investment.findfriend.global.feign.naver;
+
+import com.investment.findfriend.global.feign.dto.request.NaverTokenRequest;
+import com.investment.findfriend.global.feign.dto.response.naver.NaverTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        name = "NaverGetTokenClient",
+        url = "https://nid.naver.com/oauth2.0/authorize"
+)
+public interface NaverGetTokenClient {
+    @GetMapping
+    NaverTokenResponse execute(NaverTokenRequest request);
+}
