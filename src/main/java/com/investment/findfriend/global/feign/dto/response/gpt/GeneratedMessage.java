17e0ce0beb75ba7dc4f6fd33b57c52bfef3be344
NameIsUser06 <woosoungcho@gmail.com>
2023-12-15 09:55:01 +0900

    fix : commit 안된 파일

--- src/main/java/com/investment/findfriend/global/feign/dto/response/gpt/GeneratedMessage.java
+++ src/main/java/com/investment/findfriend/global/feign/dto/response/gpt/GeneratedMessage.java
@@ -1,8 +1,9 @@
 package com.investment.findfriend.global.feign.dto.response.gpt;
 
+import com.investment.findfriend.global.feign.dto.request.gpt.ChatGPTMessage;
 import lombok.Getter;
 
 @Getter
 public class GeneratedMessage {
-    private String text;
+    private ChatGPTMessage message;
 }
