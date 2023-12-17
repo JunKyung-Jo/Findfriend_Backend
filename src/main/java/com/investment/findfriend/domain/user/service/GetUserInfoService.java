47d840aed4479a04fde7c40b549f48d486e6a65f
NameIsUser06 <woosoungcho@gmail.com>
2023-12-17 23:11:07 +0900

    feat : friend list response 구현

--- src/main/java/com/investment/findfriend/domain/user/service/GetUserInfoService.java
+++ src/main/java/com/investment/findfriend/domain/user/service/GetUserInfoService.java
@@ -1,11 +1,13 @@
 package com.investment.findfriend.domain.user.service;
 
 import com.investment.findfriend.domain.auth.exception.UserNotFoundException;
+import com.investment.findfriend.domain.friend.presentation.dto.response.FriendResponse;
 import com.investment.findfriend.domain.user.domain.User;
 import com.investment.findfriend.domain.user.presentation.dto.response.UserResponse;
 import com.investment.findfriend.domain.user.repository.UserRepository;
 import com.investment.findfriend.global.jwt.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ public class GetUserInfoService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
+    @Transactional
     public ResponseEntity<UserResponse> execute(HttpServletRequest httpServletRequest) {
         User user = userRepository.findByEmail(jwtUtil.extractEmail(httpServletRequest)).orElseThrow(
                 () -> UserNotFoundException.EXCEPTION
@@ -24,6 +27,14 @@ public class GetUserInfoService {
         return ResponseEntity.ok(UserResponse.builder()
                 .name(user.getName())
                 .statusMessage(user.getStatusMessage())
+                .friends(user.getFriends().stream()
+                        .map(friend -> FriendResponse.builder()
+                                .id(friend.getId())
+                                .name(friend.getName())
+                                .statusMessage(friend.getStatusMessage())
+                                .authority(friend.getAuthority())
+                                .build())
+                        .toList())
                 .build());
     }
 }
