500a6da5ea4b35eb6e28e0fbc0d30741307690fa
NameIsUser06 <woosoungcho@gmail.com>
2024-01-05 01:05:59 +0900

    refactor : toString Override

--- src/main/java/com/investment/findfriend/global/error/ErrorResponse.java
+++ src/main/java/com/investment/findfriend/global/error/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.investment.findfriend.global.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,4 +10,14 @@ import lombok.Getter;
 public class ErrorResponse {
     private int status;
     private String message;
+
+    @Override
+    public String toString() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
