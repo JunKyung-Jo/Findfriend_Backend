cbe065aefc12f970c917f872918e86aa654bdb76
NameIsUser06 <woosoungcho@gmail.com>
2024-01-08 22:40:14 +0900

    refactor : delete friend

--- src/main/java/com/investment/findfriend/domain/friend/service/DeleteFriendService.java
+++ src/main/java/com/investment/findfriend/domain/friend/service/DeleteFriendService.java
@@ -1,5 +1,8 @@
 package com.investment.findfriend.domain.friend.service;
 
+import com.investment.findfriend.domain.feed.repository.FeedRepository;
+import com.investment.findfriend.domain.friend.domain.Friend;
+import com.investment.findfriend.domain.friend.exception.FriendNotFoundException;
 import com.investment.findfriend.domain.friend.repository.FriendRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteFriendService {
 
     private final FriendRepository friendRepository;
+    private final FeedRepository feedRepository;
 
     public ResponseEntity<String> execute(Long friendId) {
+        Friend friend = friendRepository.findById(friendId).orElseThrow(
+                () -> FriendNotFoundException.EXCEPTION
+        );
+        // 피드 내용 삭제
+        friend.getFeed().forEach(feed -> feedRepository.deleteById(feed.getId()));
         // 친구 id 값을 통한 삭제
         friendRepository.deleteById(friendId);
         // 성공 여부 return
